e9cd4cb86d91b56bbdc20ef3d38d9ee9f4d7aa84
ckim <ckim@godaddy.com>
2016-10-24 14:50:36 -0700

    Use handlebars and add 404 handling

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 const scApi = require('./utils/SmartCarApi');
 
 var app = express();
+var hbs = exphbs.create({
+  extname: '.hbs'
+});
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
 
 app.set('port', (process.env.PORT || 3000));
 app.use(bodyParser.json());
-app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.get('/vehicles/:id', function (req, res) {
   scApi.vehicleInfo(req.params.id, function(error, response) {
@@ -19,6 +29,8 @@ app.get('/vehicles/:id', function (req, res) {
     res.status(status).json(data);
   });
 });
+
+app.use(require('./middleware/404.js'));
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
---
+++ middleware/404.js
@@ -0,0 +1,15 @@
+module.exports = function(req, res, next) {
+  res.status(404);
+
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  if (req.accepts('json')) {
+    res.send({ error: 'Not Found' });
+    return;
+  }
+
+  res.type('txt').send('Not Found');
+};
