be8911ce45987aa0bd911b568886077835cdb1b9
calebhskim <caleb.hs.kim@gmail.com>
2016-10-26 01:00:02 -0700

    Add fuel and battery tests

--- test/e2e/routes/vehicles/vehicles.test.js
+++ test/e2e/routes/vehicles/vehicles.test.js
@@ -47,7 +47,7 @@ describe('SmartCar Routes', () => {
     describe('Given an invalid id 4321', () => {
       it('Should error with a 404 and appropriate message', (done) => {
         request(app)
-          .get(`/vehicles/${4321}/`)
+          .get(`/vehicles/${4321}/doors`)
           .expect(404)
           .end((err, res) => {
             if (err) return done(err);
@@ -62,7 +62,7 @@ describe('SmartCar Routes', () => {
     describe('Given a valid id 1234', () => {
       it('Should return correct vehicle information', (done) => {
         request(app)
-          .get(`/vehicles/${1234}/`)
+          .get(`/vehicles/${1234}/doors`)
           .expect(200)
           .end((err, res) => {
             if (err) return done(err);
@@ -87,6 +87,100 @@ describe('SmartCar Routes', () => {
             ];
 
             expect(res.body).to.eql(data);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('#GET /vehicles/:id/fuel', () => {
+    describe('Given an invalid id 4321', () => {
+      it('Should error with a 404 and appropriate message', (done) => {
+        request(app)
+          .get(`/vehicles/${4321}/fuel`)
+          .expect(404)
+          .end((err, res) => {
+            if (err) return done(err);
+
+            const { message } = res.body;
+            expect(message).to.eql('Vehicle id: 4321 not found.');
+            done();
+          });
+      });
+    });
+
+    describe('Given a valid id 1235 that does not have fuel', () => {
+      it('Should return 403 with appropriate message', (done) => {
+        request(app)
+          .get(`/vehicles/${1235}/fuel`)
+          .expect(403)
+          .end((err, res) => {
+            if (err) return done(err);
+            
+            const { message } = res.body;
+            expect(message).to.eql('Vehicle with id 1235 does not have fuel.');
+            done();
+          });
+      });
+    });
+
+    describe('Given a valid id 1234', () => {
+      it('Should return correct vehicle fuel information', (done) => {
+        request(app)
+          .get(`/vehicles/${1234}/fuel`)
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done(err);
+
+            const { percentage } = res.body;
+            expect(percentage).to.eql('99.1');
+            done();
+          });
+      });
+    });
+  });
+
+  describe('#GET /vehicles/:id/battery', () => {
+    describe('Given an invalid id 4321', () => {
+      it('Should error with a 404 and appropriate message', (done) => {
+        request(app)
+          .get(`/vehicles/${4321}/battery`)
+          .expect(404)
+          .end((err, res) => {
+            if (err) return done(err);
+
+            const { message } = res.body;
+            expect(message).to.eql('Vehicle id: 4321 not found.');
+            done();
+          });
+      });
+    });
+
+    describe('Given a valid id 1234 that does not have battery', () => {
+      it('Should return 403 with appropriate message', (done) => {
+        request(app)
+          .get(`/vehicles/${1235}/battery`)
+          .expect(403)
+          .end((err, res) => {
+            if (err) return done(err);
+            
+            const { message } = res.body;
+            expect(message).to.eql('Vehicle with id 1235 does not have battery.');
+            done();
+          });
+      });
+    });
+
+    describe('Given a valid id 1234', () => {
+      it('Should return correct vehicle battery information', (done) => {
+        request(app)
+          .get(`/vehicles/${1234}/battery`)
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done(err);
+
+            const { percentage } = res.body;
+            expect(percentage).to.eql('v8');
             done();
           });
       });
