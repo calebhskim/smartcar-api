681e54f06b474a8bc7dd512e28c1225ae0320354
calebhskim <caleb.hs.kim@gmail.com>
2016-10-28 02:05:23 -0700

    Add rate limiting

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import exphbs from 'express-handlebars';
 import bodyParser from 'body-parser';
+import rateLimit from 'express-rate-limit';
 import config from  './config';
 
 const app = module.exports = express();
 const hbs = exphbs.create({
   extname: '.hbs',
 });
+const apiLimiter = new rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes,
+  delayAfter: 20,
+  max: 25,
+});
 
 app.set('env', process.env.NODE_ENV || 'development');
 app.set('port', (process.env.PORT || config.http));
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
+app.use('/vehicles/', apiLimiter);
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
